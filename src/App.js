c181a971301b33053f089ef42f1dec733f4bec86
lvyangdan <2737920271@qq.com>
2017-08-02 10:09:41 +0800

    second

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+
+  handleChange = (event) => {
+    const file = event.target.files[0]
+    const reader = new FileReader()
+    reader.onload = (event) => {
+      console.log(event.target.result)
+      let data = {
+        file: event.target.result
+      }
+
+      //  数据挂到 form 上才能发送
+      let formData = new FormData()
+      formData.append('avatar', file)
+      // 这样，可以保证 multipart/form-data 
+
+
+      axios.post(`http://192.168.0.119:3001/upload/avater`, formData).then(
+        res=> {
+
+        }
+      )
+      // 只有在这里，执行 file 相关操作，那么 file 里面才是有真正数据的
+    }
+    reader.readAsDataURL(file)
+    console.log('hello...', file)
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="file" onChange={this.handleChange}
+          className="file-upload-input" />
       </div>
     );
   }
